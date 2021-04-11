df5d2dbd0ecafc4ec4d03771c07b0ae512cb159a
P3ndalf <52200518+P3ndalf@users.noreply.github.com>
2021-04-11 15:26:25 +0300

    Enlarge+Timer

--- Scripts/Timer.cs
+++ Scripts/Timer.cs
@@ -1,18 +1,44 @@
-п»їusing System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //раскомментировать, если понадобится задавать иное время до поражения на вход 
+    public int inputMinute; 
+    public int inputSecond; 
+    
+    private float timeInSecondsP;
+    public static int minutsP;
+    public static int secondsP;
+    public Text minutes;
+    public Text seconds;
 
-    // Update is called once per frame
+    private void Start()
+    {
+        //timeInSecondsP = 420;// На вход подается 7 минут
+        timeInSecondsP = inputMinute * 60 + inputSecond; //Раскомментировать,если понадобится на вход подавать иное время до поражения
+    }
     void Update()
     {
-        
+        if(timeInSecondsP >= 0 ) // Дописать условие из библиотеки!!!!! Если ходит другой игрок - таймер замирает
+        {
+            timeInSecondsP -= Time.deltaTime;
+            secondsP = (int)(timeInSecondsP % 60); 
+            minutsP = (int)(timeInSecondsP / 60); 
+            if (minutsP < 10) // Чтобы значение <10 в строке  не теряли 0 в старшем разряде 
+                minutes.text = 0 + minutsP.ToString();
+            else 
+                minutes.text = minutsP.ToString();
+            if (secondsP < 10)
+                seconds.text = 0 + secondsP.ToString(); 
+            else
+                seconds.text = secondsP.ToString();
+        }
     }
-}
+    /*
+    bool isMyTurn() {
+        return () ? true : false;
+    }
+    */
+ }
