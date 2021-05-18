bd69032083b3d4c51a678fddf7e106a7fc88b6f1
P3ndalf <52200518+P3ndalf@users.noreply.github.com>
2021-05-18 21:03:24 +0300

    Update HelpBox.cs

--- Scripts/HelpBox.cs
+++ Scripts/HelpBox.cs
@@ -7,37 +7,48 @@ using System.Threading;
 public class HelpBox : MonoBehaviour
 {
     int milliseconds = 0;
+    const int timeUntilDeath = 3000;
     int counter = 0;
+    float alpha = 0f;
     const int arrSize = 4;
     GameObject helpBox;
     GameObject imageField;
-    GameObject tempImages;
-    List<Sprite> images = new List<Sprite>() { };// Start is called before the first frame update
+    List<Sprite> images = new List<Sprite>() { };
+
     private void Start()
     {
         DownloadingImages(arrSize);
         helpBox = GameObject.Find("HelpBox");
-        tempImages = GameObject.Find("Images");
         imageField = new GameObject();
-        imageField.transform.SetParent((helpBox.transform.GetChild(0)).transform.GetChild(0));
+        imageField.transform.SetParent((helpBox.transform.GetChild(0)));
+        imageField.transform.localPosition = new Vector2(0, 0);
+        imageField.transform.localScale = new Vector2(2.7f, 3);
         imageField.AddComponent<Image>();
         imageField.GetComponent<Image>().sprite = images[0];
+        imageField.GetComponent<Image>().color = new Color(1f, 1f, 1f, 0);
     }
     private void Update()
     {
         if (helpBox.activeSelf)
         {
             milliseconds++;
-            if (milliseconds == 3000)
+            if (milliseconds < timeUntilDeath / 2)
+                alpha += (float)10 / timeUntilDeath;
+            else
+                alpha -= (float)10 / timeUntilDeath;
+
+            imageField.GetComponent<Image>().color = new Color(1f,1f,1f,alpha);
+            if (milliseconds == timeUntilDeath)
             {
+                alpha = 0f;
                 if (counter == arrSize - 1) counter = 0;
                 else counter++;
                 imageField.GetComponent<Image>().sprite = images[counter];
                 milliseconds = 0;
-            }
+            }    
         }
     }
-    void DownloadingImages(int _arrSize)
+    private void DownloadingImages(int _arrSize)
     {
         string path = "HelpImages/";
         for (int i = 0; i < _arrSize; i++)
