548c0372112eb8386b39aeee1505c07c5b8b302e
P3ndalf <52200518+P3ndalf@users.noreply.github.com>
2021-05-20 16:04:30 +0300

    Yes

--- Scripts/SideChoice.cs
+++ Scripts/SideChoice.cs
@@ -17,32 +17,37 @@ public class SideChoice : MonoBehaviour
         gameChoice.transform.localPosition = _position;
         gameChoice.transform.localScale = _scale;
         gameChoice.AddComponent<Image>();
-        gameChoice.GetComponent<Image>().sprite = Resources.Load<Sprite>(@"Chess\wSquare");
-        gameChoice.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+        gameChoice.GetComponent<Image>().sprite = (_name == "Black") ? Resources.Load<Sprite>(@"Fonts/Dark_Side") : Resources.Load<Sprite>(@"Fonts/Light_Side");
         gameChoice.AddComponent<Button>();
-        var colors = gameChoice.GetComponent<Button>().colors;
+        gameChoice.GetComponent<Button>().transition = Selectable.Transition.SpriteSwap;
+        var sprites = gameChoice.GetComponent<Button>().spriteState;
         switch (gameChoice.name) {
             case "White":
-                colors.normalColor = Color.white;
-                colors.highlightedColor = Color.blue;
-                colors.selectedColor = Color.white;
-                colors.pressedColor = Color.white;
+                sprites.highlightedSprite = Resources.Load<Sprite>(@"Fonts/Light_Side_High");
+                sprites.disabledSprite = Resources.Load<Sprite>(@"Fonts/Light_Side");
+                sprites.selectedSprite = Resources.Load<Sprite>(@"Fonts/Light_Side");
+                sprites.disabledSprite = Resources.Load<Sprite>(@"Fonts/Light_Side");
                 break;
             case "Black":
-                colors.normalColor = Color.black;
-                colors.highlightedColor = Color.red;
-                colors.selectedColor = Color.black;
-                colors.pressedColor = Color.black;
+                sprites.highlightedSprite = Resources.Load<Sprite>(@"Fonts/Dark_Side_High");
+                sprites.disabledSprite = Resources.Load<Sprite>(@"Fonts/Dark_Side");
+                sprites.selectedSprite = Resources.Load<Sprite>(@"Fonts/Dark_Side");
+                sprites.disabledSprite = Resources.Load<Sprite>(@"Fonts/Dark_Side");
                 break;
         }
-        gameChoice.GetComponent<Button>().colors = colors;
+        gameChoice.GetComponent<Button>().spriteState = sprites;
     }
     void Start()
     {
+        Vector2 scaleW = new Vector2(2.5f, 4.5f);
+        Vector2 positionW = new Vector2(-132.79f, 12.06f);
+        Vector2 scaleB = new Vector2(2.5f, 4.5f);
+        Vector2 positionB = new Vector2(114.3f, 12.06f);
+
         result = GameObject.Find("ResultSideChoice");
         Debug.Log(result.name);
-        ChoiceW = new SideChoice("White",new Vector2(2.5f, 4.5f), new Vector2(-132.79f, 12.06f));
-        ChoiceB = new SideChoice("Black",new Vector2(2.5f, 4.5f), new Vector2(114.3f, 12.06f));
+        ChoiceW = new SideChoice("White",scaleW, positionW);
+        ChoiceB = new SideChoice("Black",scaleB, positionB);
         ChoiceW.gameChoice.GetComponent<Button>().onClick.AddListener(WhiteChoice);
         ChoiceB.gameChoice.GetComponent<Button>().onClick.AddListener(BlackChoice);
 
